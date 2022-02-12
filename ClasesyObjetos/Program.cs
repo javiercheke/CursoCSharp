8459c81cfb925472b1211e2dd54f89a8f8bb96c4
javiercheke <tafurj@gmail.com>
2022-02-12 20:35:17 +0100

    JTP: ClasesyObjetos

---
+++ ClasesyObjetos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClasesyObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clase clase1 = new Clase();
+            clase1.Metodo();             
+        }
+    }
+
+    class  Clase{
+        public void Metodo(){
+
+            console.writeline("esto es un metodo");
+        }
+    }
+}
